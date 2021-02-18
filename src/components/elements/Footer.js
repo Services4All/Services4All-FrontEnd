a7df5440dc80871cda9227b643d2fb086be21c43
JuanRomero11 <juan.romero-si@mail.escuelaing.edu.co>
2021-02-18 14:18:39 -0500

    :sparkles:

---
+++ src/components/elements/Footer.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col">
+            <h4>Services4All</h4>
+           <ui className="list-unstyled">
+              <li>Escuela Colombiana de ingenieria Julio Garavito</li>
+            </ui>
+          </div>
+          {/* Column2 */}
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col">
+            <h4>Equipo</h4>
+            <ui className="list-unstyled">
+              <li>Esteban Bernal</li>
+              <li>Andres Sotelo</li>
+              <li>Juan Romero</li>
+              <li>Nicolas Ortega</li>
+              <li>German Ospina</li>
+            </ui>
+          </div>
+          {/* Column3 */}
+          
+           <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col">
+            <h4>Contac us</h4>
+            <ui className="list-unstyled">
+              <a
+          className="App-link"
+          href="https://github.com/Services4All/Services4All-FrontEnd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <li>Github</li>
+        </a>
+               <a
+          className="App-link"
+          href="https://www.instagram.com/?hl=es"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+            <li>Instagram</li>
+        </a>
+             <a
+          className="App-link"
+          href="https://twitter.com/?lang=es"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+            <li>Twitter</li>
+        </a>
+            </ui>
+          </div>
+        </div>
+
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} Services4All | All rights reserved |
+            Terms Of Service | 24 hours support | Privacy
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
