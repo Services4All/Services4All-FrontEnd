8692f7cb68cbc50c1b352ce7ea526c9e95f99055
EstebanK23 <Estebanb0203@hotmail.com>
2021-02-23 23:25:22 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import login from './components/login';
+import about from './components/about';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 ReactDOM.render(
    <BrowserRouter>
       <Switch>
         <Route exact path="/Login" component={login} />
+        <Route exact path="/About" component={about} />
       </Switch>
   </BrowserRouter>,
   document.getElementById('root')
