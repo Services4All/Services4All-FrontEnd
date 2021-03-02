3d9e641ef44eb7376ca78262f66f7e3a43d9ad26
Nicolas Ortega <nicolas.ortega-l@mail.escuelaing.edu.co>
2021-03-02 18:17:42 -0500

    :white_check_mark: Testing Main y Servicios

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,64 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Servicios from './components/servicios';
+import Main from "./components/main"
 
-test('renders learn react link', () => {
+test('renders Principal', () => {
   render(<App />);
   const linkElement = screen.getByText(/Categorías de servicios/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+it('Test Servicios 1', () => {
+
+
+   const { container, getByText } = render(<Servicios />);
+
+   expect(getByText(/Consulta/i).textContent)
+
+
+})
+
+it('Test Servicios 2', () => {
+
+
+   const { container, getByText } = render(<Servicios />);
+
+   expect(getByText(/Servicio de Plomería Barrio Ciudadela Colsubsidio/i).textContent)
+
+
+})
+
+it('Test Servicios 3', () => {
+
+
+   const { container, getByText } = render(<Servicios />);
+
+   expect(getByText(/Servicio de Cerrajería Barrio Villas de Gratamira/i).textContent)
+
+
+})
+
+
+it('Test Main 1', () => {
+
+
+   const { container, getByText } = render(<Main />);
+
+   expect(getByText(/Categorías de servicios/i).textContent)
+
+
+})
+
+it('Test Main 2', () => {
+
+
+   const { container, getByText } = render(<Main />);
+
+   expect(getByText(/Carpintería/i).textContent)
+
+
+})
+
