e720adc35830bf1062966b3d92e18bfcf7d363f8
Nicolas Ortega <nicolas.ortega-l@mail.escuelaing.edu.co>
2021-03-02 17:02:54 -0500

    :bug: Servicios duplicate fix

--- src/components/servicios.js
+++ src/components/servicios.js
@@ -1,6 +1,4 @@
-  //Dependencies
 import React, { useState, handleOpen } from "react";
-//import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
@@ -10,54 +8,31 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
-//import Modal from "@material-ui/core/Modal";
-//import './servicios.css';
 import Footer from "./elements/Footer";
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: "white",
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
+  view: {
     height: "100%",
     display: "flex",
     flexDirection: "column",
   },
-  cardMedia: {
+  sampleMedia: {
     paddingTop: "56.25%", // 16:9
+  }, 
+  sampleGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
   },
-  cardContent: {
+  sampleContent: {
     flexGrow: 1,
   },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-  paper: {
-    position: "absolute",
-    width: 600,
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
+  
 }));
 
 function Setup(){
 	const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	const classes = useStyles();
-	const [cardName] = useState([
+	const [namesArray] = useState([
 		"Servicio de Carpintería Osorio Barrio Alamos",
 		"Servicio de Plomería Barrio Ciudadela Colsubsidio",
 		"Servicio de Mecánica Daytona Barrio Mirandela",
@@ -71,7 +46,7 @@ function Setup(){
 		"Servicio de Consultoría médica Barrio Quirigua",
 		"Servicio de Cerrajería Barrio Villas de Gratamira",
 	]);
-	const [cardImage] = useState([
+	const [imgArray] = useState([
 		"https://www.tumercadoclick.com/web/templates/_tmcPhotos/TMC_Z3u7uk9wp8Y7nB3tRLLcaGwI_hD85DbvXASs2pzVU_20200316213433_0.png",
 		"https://www.plomerosyreparaciones.com/images/plomeros-bogota.jpg",
 		"https://daytonautocol.com/wp-content/uploads/2019/01/Daytona-Centro-Servicio-Automotriz-bogota-logo.png",
@@ -88,19 +63,19 @@ function Setup(){
 	return(
 		<div>
           <h1> Consulta aquí nuestra oferta de servicios:</h1>
-		  <Container className={useStyles().cardGrid} maxWidth="md">
+		  <Container className={useStyles().sampleGrid} maxWidth="md">
         <Grid container spacing={4}>
           {cards.map((card) => (
             <Grid item key={card} xs={12} sm={6} md={4}>
-              <Card className={classes.card}>
+              <Card className={classes.view}>
                 <CardMedia
-                  className={classes.cardMedia}
-                  image={cardImage[card - 1]}
-                  title="Image title"
+                  className={classes.sampleMedia}
+                  image={imgArray[card - 1]}
+                  title="Title"
                 />
-                <CardContent className={classes.cardContent}>
+                <CardContent className={classes.sampleContent}>
                   <Typography gutterBottom variant="h5" component="h2">
-                    {cardName[card - 1]}
+                    {namesArray[card - 1]}
                   </Typography>
                 </CardContent>
                 <CardActions>
