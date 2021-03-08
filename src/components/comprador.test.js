420e8eb205cc30e131ef10722f3a11e7591be76d
Oscar Mendez <oscar.mendez@avaldigitallabs.com>
2021-03-08 16:32:45 -0500

    :wrench: Add unit test

--- src/components/comprador.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Comprador from './comprador';
-
-test('renders Principal', () => {
-    render(<Router><Comprador /></Router>);
-    const divElement = screen.getByText(/other code/i);
-    expect(divElement).toBeInTheDocument();
-  });
--- src/components/newComponent.js
+++ src/components/newComponent.js
@@ -5,7 +5,7 @@ import NavBar from './navBar/NavBar';
 import {NavLink} from 'react-router-dom';
 import './comprador.css';
 
-export default class comprador extends React.Component {
+export default class NewComponent extends React.Component {
   render() {
     return(
       <div className="comprador">
@@ -13,7 +13,7 @@ export default class comprador extends React.Component {
           <NavBar />
         </div>
         <div className="comprador-title">
-          <h1>BIENVENIDO COMPRADOR</h1>
+          <h1>BIENVENIDO NUEVO COMPONENTE</h1>
         </div>
         <div className="itemsdos">
             <p><NavLink activeClassName="selected" className="nav-link-header" to="/">Favoritos</NavLink></p>
