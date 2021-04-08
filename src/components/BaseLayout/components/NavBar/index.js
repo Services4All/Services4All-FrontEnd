2af54ff234794082f53454174dc05f7a63c0047a
Nicolas Ortega <nicolas.ortega-l@mail.escuelaing.edu.co>
2021-04-08 17:02:17 -0500

    :sparkles: Historia#60

--- src/components/BaseLayout/components/NavBar/index.js
+++ src/components/BaseLayout/components/NavBar/index.js
@@ -24,7 +24,6 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import TextField from "@material-ui/core/TextField";
 
 // Internals
 import "./index.css";
