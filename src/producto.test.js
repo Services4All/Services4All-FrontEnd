1c1cf950ed154f145a180a127e1494ec28775ab8
Nicolas Ortega <nicolas.ortega-l@mail.escuelaing.edu.co>
2021-05-20 19:00:57 -0500

    :sparkles: Historia-108

--- src/producto.test.js
+++ src/producto.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Producto from "./components/producto";
 
+
 test('renders new component', () => {
     render(<Router><Producto /></Router>);	
     const divElement = screen.getAllByText(/Comentario/i);
